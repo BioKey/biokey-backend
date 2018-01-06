90176f37103ee7866e18ff79e25f9c2218939bbe
mkucera3 <mkucera3@uwo.ca>
2018-01-06 11:27:26 -0500

    One more

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,30 +1,1 @@
-define({
-  "name": "biokey-backend",
-  "version": "0.0.0",
-  "description": "backend for biokey security system",
-  "title": "BioKey Documentation",
-  "url": "https://service.biokey.com",
-  "template": {
-    "withCompare": true,
-    "withGenerator": true
-  },
-  "order": [
-    "Register",
-    "Login",
-    "Me",
-    "ListUsers",
-    "GetUser",
-    "UpdateUser",
-    "DeleteUser",
-    "ListActivities"
-  ],
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2017-12-10T18:50:17.441Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "name": "biokey-backend",  "version": "0.0.0",  "description": "backend for biokey security system",  "title": "BioKey Documentation",  "url": "https://service.biokey.com",  "template": {    "withCompare": true,    "withGenerator": true  },  "order": [    "Register",    "Login",    "Me",    "ListUsers",    "GetUser",    "UpdateUser",    "DeleteUser"  ],  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-01-06T16:27:14.578Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
